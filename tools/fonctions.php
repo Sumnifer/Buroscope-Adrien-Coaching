a2a22f08522c410b16fd6f47c9e48c4d92d90619
Sumnifer <114100714+Sumnifer@users.noreply.github.com>
2023-04-24 23:38:21 +0200

    Security Update

--- tools/fonctions.php
+++ tools/fonctions.php
@@ -13,23 +13,30 @@ function connexion()
 function login($login, $pass)
 {
     $connexion = connexion();
-    $requete = "SELECT * FROM users WHERE email_users='" . $login . "'";
-    $resultat = mysqli_query($connexion, $requete);
+    $requete = "SELECT * FROM users WHERE email_users=?";
+    $stmt = mysqli_prepare($connexion, $requete);
+    mysqli_stmt_bind_param($stmt, 's', $login);
+    mysqli_stmt_execute($stmt);
+    $resultat = mysqli_stmt_get_result($stmt);
     $nb_ligne = mysqli_num_rows($resultat);
     if ($nb_ligne == 1) {
         $ligne = mysqli_fetch_object($resultat);
-        $hashed_pass = PASSWORD_VERIFY($pass, $ligne->pass_users);
-        $_SESSION["id_users"] = $ligne->id_users;
-        $_SESSION["name_users"] = $ligne->name_users;
-        $_SESSION["surname_users"] = $ligne->surname_users;
-        $_SESSION["email_users"] = $ligne->email_users;
-        $_SESSION["statut_users"] = $ligne->statut_users;
-        if ($ligne->statut_users == "root"){
-            header("location: ../back/back.php");
-        }else {
-            header("location: ../front/front.php?action=index");
+        $hashed_pass = password_verify($pass, $ligne->pass_users);
+        if ($hashed_pass) {
+            $_SESSION["id_users"] = $ligne->id_users;
+            $_SESSION["name_users"] = $ligne->name_users;
+            $_SESSION["surname_users"] = $ligne->surname_users;
+            $_SESSION["email_users"] = $ligne->email_users;
+            $_SESSION["statut_users"] = $ligne->statut_users;
+            if ($ligne->statut_users == "root"){
+                header("location: ../back/back.php");
+            }else {
+                header("location: ../front/front.php?action=index");
+            }
         }
     }
     mysqli_close($connexion);
 }
+
+
 
