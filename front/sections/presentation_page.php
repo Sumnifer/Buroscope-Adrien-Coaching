fcbe724ee5367857cbcac889089923fc79ed9442
Sumnifer <114100714+Sumnifer@users.noreply.github.com>
2023-04-25 13:43:08 +0200

    Sliders Introduce

--- front/sections/presentation_page.php
+++ front/sections/presentation_page.php
@@ -5,8 +5,19 @@ mysqli_set_charset($connexion, "utf8");
 $request = "SELECT * FROM presentations WHERE visibility_presentations != 2  ORDER BY rank_presentations ASC";
 $result=mysqli_query($connexion, $request);
 
+$request1 = "SELECT * FROM sliders where visibility_sliders=1";
+$result1=mysqli_query($connexion, $request1); ?>
+<div class="slider">
+    <?php
+    while ($rows1=mysqli_fetch_object($result1)){
+  echo "<img class='slider-image' src='".$rows1->img_sliders."' alt='".$rows1->alt_sliders."'>"; }
+  ?>
+  <button id="slider-prev"><i class="fa-solid fa-chevron-left slider_icons"></i></button>
+  <button id="slider-next"><i class="fa-solid fa-chevron-right slider_icons"></i></button>
 
+</div>
 
+<?php
     $content = "<main class='presentation_page' >";
     $content .= "<h1 class='presentation_page__title'> Mes différentes <span class='presentation_page__title_span'>presentations</span></h1>";
      while($rows = mysqli_fetch_object($result)) {
